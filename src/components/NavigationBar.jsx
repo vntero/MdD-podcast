925d30b2ef0abb54cedf81aca66a0ef968cdca91
vntero <shin.antero@gmail.com>
2022-02-10 14:15:55 +0100

    navbar toggle removed

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function NavigationBar() {
@@ -7,12 +7,12 @@ function NavigationBar() {
     <Navbar bg="light" expand="lg" sticky='top'>
       <Container>
         <Navbar.Brand><Link to='/'><img src="/images/microphone.png" alt="MdD" height={35} width={35}/></Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             
           </Nav>
-        </Navbar.Collapse>
+        </Navbar.Collapse> */}
       </Container>
     </Navbar>
     
