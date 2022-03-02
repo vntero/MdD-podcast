45c20a6c4901b66453afd2c3ebabc51af3fbe931
vntero <shin.antero@gmail.com>
2022-03-02 19:36:38 +0100

    material ui installed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+//google fonts 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+//essentials
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
